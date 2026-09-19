79ce1237c458b6f05b93e1eca56950b1b59e7032
rangga <ranggashadow27@gmail.com>
2026-09-19 11:17:56 +0700

    Fix link problem

--- data/invitationTemplates.ts
+++ data/invitationTemplates.ts
@@ -53,7 +53,7 @@ Detail acara dan lokasi selengkapnya dapat diakses melalui tautan berikut:
 🔗 *Link Undangan :* 
 {link}
 
-Kehadiran serta doa restu dari {nama} sangat berarti bagi kami. Terima kasih dan sampai jumpa! 🙌`,
+Kehadiran serta doa restu dari anda sangat berarti bagi kami. Terima kasih dan sampai jumpa! 🙌`,
   },
   {
     id: "singkat",
